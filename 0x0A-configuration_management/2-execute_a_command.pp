1fbbca3ebd69869a003e588e76407a2912fa2658
DuncanMoyo <duncanfmoyo@gmail.com>
2023-09-22 22:04:18 +0000

    puppet lint error

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 # create a manifest that kills a process named killmenow.
 
 exec { 'killmenow':
-  command     => 'pkill -f killmenow',
-  path        => '/bin:/usr/bin:/usr/local/bin',
-  onlyif      => 'pgrep -f killmenow'
-}
+  command => 'pkill -f killmenow',
+  path    => '/bin:/usr/bin:/usr/local/bin',
+  onlyif  => 'pgrep -f killmenow' }
