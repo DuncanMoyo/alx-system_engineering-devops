ea5287e5a4fac169229cc8f1222643e2618d933c
Duncan Moyo <duncanfmoyo@gmail.com>
2023-11-08 12:32:40 +0000

    Task 0 - replaced user agent with more meaningful description

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,9 +5,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'custom'}
+    headers = {'User-Agent': 'deeIsProbing'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
         return 0
-    return response.json()['data']['subscribers']
+    return response.json()['data']['subscribers'
