23ee98900ad51343000287ece3d796d1890a480f
Duncan Moyo <duncanfmoyo@gmail.com>
2023-11-08 13:01:16 +0000

    Task 1 - rewritten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+"""
+function that queries the reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit.
+"""
 
-"""
-function that queries the Reddit API and prints the titles of t
-he first 10 hot posts listed for a given subreddit.
-"""
 import requests
 
 
@@ -11,18 +11,20 @@ def top_ten(subreddit):
     """
     prints first 10 hot posts for given subreddit
     """
-
     if subreddit is None or type(subreddit) is not str:
         print("None")
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'deeIsProbing'}
+    headers = {
+        'User-Agent': 'deeIsProbing'
+    }
+
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code != 200:
-        print("None")
-        return
+    if response.status_code == 200:
+        posts = response.json()['data']['children']
+        for post in posts:
+            print(post['data']['title'])
 
-    data = response.json()['data']['children']
-    for post in data[:10]:
-        print(post['data']['title'])
+    else:
+        print("None")
