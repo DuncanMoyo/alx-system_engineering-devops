eaab5bac2ec6ac4bd3c81783c3240640c4030360
Duncan Moyo <duncanfmoyo@gmail.com>
2023-11-08 12:47:19 +0000

    Task 1 - Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+function that queries the Reddit API and prints the titles of t
+he first 10 hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    prints first 10 hot posts for given subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'deeIsProbing'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        print(None)
+        return
+
+    data = response.json()['data']['children']
+    for post in data[:10]:
+        print(post['data']['title'])
