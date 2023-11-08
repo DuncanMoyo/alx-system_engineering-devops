6c2b0b23b48a5a4acd240fff1e3301b0cafca28c
Duncan Moyo <duncanfmoyo@gmail.com>
2023-11-08 13:12:49 +0000

    Task 3 - Count it!

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API, parses the title of all hot articles,
+and prints a sorted count of given keywords (case-insensitive,
+delimited by spaces. Javascript should count as javascript,
+but java should not).
+"""
+
+from collections import defaultdict
+import requests
+
+
+def count_words(subreddit, word_list, after=None,
+                word_counts=defaultdict(int)):
+    """
+    parses the title of all hot articles,
+    prints a sorted count of given keywords.
+    """
+    headers = {
+        'User-Agent': 'script to parse all titles by u/deano_southafrican'
+    }
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+    if after:
+        url += "?after={}".format(after)
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        if after is None:
+            return
+        else:
+            sorted_words = sorted(word_counts.items(),
+                                  key=lambda x: (-x[1], x[0]))
+            for word, count in sorted_words:
+                if count > 0:
+                    print("{}: {}".format(word, count))
+            return
+
+    data = response.json()['data']
+    titles = [post['data']['title'] for post in data['children']]
+
+    for title in titles:
+        title_words = title.split()
+        for word in word_list:
+            word_counts[word.lower()] += title_words.count(word)
+            for title_word in title_words:
+                if title_word.lower().rstrip('!?.') == word.lower():
+                    word_counts[word.lower()] += 1
+
+    if data['after']:
+        return count_words(subreddit, word_list, data['after'], word_counts)
+    else:
+        sorted_words = sorted(word_counts.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in sorted_words:
+            if count > 0:
+                print("{}: {}".format(word, count))
