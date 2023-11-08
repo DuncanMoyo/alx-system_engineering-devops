6d3808d5c39c5be84d5805f6461667496cf9de67
Duncan Moyo <duncanfmoyo@gmail.com>
2023-11-08 12:39:33 +0000

    Task 0 - added comments

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
+"""
+returns the number of subscribers (not active users, total subscribers)
+for a given subreddit.
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """
+    returns number of subs for the given subreddit.
+    """
+    
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'deeIsProbing'}
     response = requests.get(url, headers=headers, allow_redirects=False)
